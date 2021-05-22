9f6cca18ea366a3b2a9901566e30de5318336c7d
MaximeArn <maxime-arnould@outlook.fr>
2021-05-22 14:43:58 +0200

    create the footer

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../containers/Header";
 import Articles from "../Articles/Articles";
+import Footer from "../Footer/Footer";
 import "./App.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Articles />
+      <Footer />
     </div>
   );
 }
