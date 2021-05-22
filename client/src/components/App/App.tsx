af10811828fac5b2631a3e242a8419bad5701a20
MaximeArn <maxime-arnould@outlook.fr>
2021-05-22 13:54:03 +0200

    create Articles component

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../containers/Header";
+import Articles from "../Articles/Articles";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Articles />
     </div>
   );
 }
---
+++ client/src/components/Articles/Articles.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import tshirt from "./tshirt.jpeg";
+
+const Articles = () => {
+  return (
+    <div classname="articles">
+      <img src={tshirt} alt="purple tshirt" />
+    </div>
+  );
+};
+
+export default Articles;
